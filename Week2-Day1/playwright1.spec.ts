9e444597a7e059ae7b2ce254df3dde4ebb6f069a
Dimplegurajala <dimplegurajala@gmail.com>
2026-03-24 15:56:23 +0530

    CI: Week2-Day1- Playwright1

---
+++ Week2-Day1/playwright1.spec.ts
@@ -0,0 +1,26 @@
+import {chromium, webkit, test} from "@playwright/test"
+test.describe.configure({ mode: 'parallel'})
+
+test("1.launch redbus in Edge", async() =>{
+    const browserInstance = await chromium.launch({headless: false, channel : "msedge"})
+    const browserContext = await browserInstance.newContext()
+    const page = await browserContext.newPage()
+    await page.goto("https://www.redbus.in")
+    console.log(page.title())
+    console.log(page.url())
+    await page.waitForTimeout(5000) 
+    await browserInstance.close()   
+})
+
+
+test("2.launch flipkart in Webkit", async() =>{
+    const browserInstance = await webkit.launch({headless: false})
+    const browserContext = await browserInstance.newContext()
+    const page = await browserContext.newPage()
+    await page.goto("https://www.flipkart.com")
+    console.log(page.title())
+    console.log(page.url())
+    await page.waitForTimeout(5000)
+    await browserInstance.close()    
+})
+
